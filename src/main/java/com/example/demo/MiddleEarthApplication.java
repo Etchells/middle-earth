caca8ce39c4ee0624c2f84777334128480b6f385
Etchells <glenn.etchells@outlook.com>
2022-05-30 14:35:23 +0100

    First commit

---
+++ src/main/java/com/example/demo/MiddleEarthApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleEarthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleEarthApplication.class, args);
+	}
+
+}
